c534e9fc2db2366be3f714b1168952eb1dad1499
PhilipGeil <pgj.philip@gmail.com>
2020-12-15 13:04:02 +0100

    Initial commit

---
+++ Flaskeautomaten/Buffer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Flaskeautomaten
+{
+    class Buffer
+    {
+        Queue<string> products = new Queue<string>();
+
+        public void AddProduct(string product)
+        {
+            Thread.Sleep(2000);
+            lock (products)
+            {
+                while (products.Count == 10)
+                {
+                    Console.WriteLine("{0} - Waiting to add product", Thread.CurrentThread.Name);
+                    Monitor.Wait(products);
+                }
+
+
+                products.Enqueue(product);
+                //Console.WriteLine("{0} - added a {1}",Thread.CurrentThread.Name, product);
+                Monitor.PulseAll(products);
+            }
+        }
+
+        public string PullProduct()
+        {
+            string product;
+            Thread.Sleep(2000);
+            lock (products)
+            {
+                while (products.Count == 0)
+                {
+                    Console.WriteLine("{0} - Waiting to pull product", Thread.CurrentThread.Name);
+                    Monitor.Wait(products);
+                }
+
+
+                product = products.Dequeue();
+                Monitor.PulseAll(products);
+            }
+            return product;
+        }
+    }
+}
---
+++ Flaskeautomaten/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Flaskeautomaten
+{
+    class Program
+    {
+        static Buffer buffer1 = new Buffer();
+        static Buffer bufferSoda = new Buffer();
+        static Buffer bufferBeer = new Buffer();
+        static void Main(string[] args)
+        {
+            Thread producer = new Thread(Producer);
+            Thread splitter = new Thread(Splitter);
+            Thread conSoda = new Thread(Consumer);
+            Thread conBeer = new Thread(Consumer);
+            producer.Name = "Producer";
+            splitter.Name = "Splitter";
+            conSoda.Name = "Soda Consumer";
+            conBeer.Name = "Beer Consumer";
+
+            producer.Start();
+            splitter.Start();
+            conSoda.Start(bufferSoda);
+            conBeer.Start(bufferBeer);
+        }
+
+        static void Producer()
+        {
+            List<string> bottles = new List<string>() { "Sodavand", "Øl" };
+            Random rand = new Random();
+            while (true)
+            {
+                string product = bottles[rand.Next(0, bottles.Count)];
+                buffer1.AddProduct(product);
+            }
+        }
+
+        static void Splitter()
+        {
+            while (true)
+            {
+                string product = buffer1.PullProduct();
+
+                if (product == "Sodavand")
+                {
+                    bufferSoda.AddProduct(product);
+                    Console.WriteLine("The splitter added a soda to the soda buffer");
+                } else
+                {
+                    bufferBeer.AddProduct(product);
+                    Console.WriteLine("The splitter added a beer to the beer buffer");
+                }
+            }
+        }
+
+        static void Consumer(object buffer)
+        {
+            Buffer buf = buffer as Buffer;
+            while (true)
+            {
+                Thread.Sleep(5000);
+                string product = buf.PullProduct();
+                Console.WriteLine("{0} has pulled a {1} from the machine", Thread.CurrentThread.Name, product);
+            }
+        }
+    }
+}
